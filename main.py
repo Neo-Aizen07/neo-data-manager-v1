6ffa6ab4289ced78bc48e5029f39dc9fec9446b8
Neo-Aizen07 <manoj1207m@gmail.com>
2026-01-29 13:33:23 +0530

    Update to v1.1: Fixed and optimized logic

--- main.py
+++ main.py
@@ -7,57 +7,58 @@ import os
 import uuid
 class RecordManager():
     def __init__(self): 
-        self.name_folder_1={}
+        self.records={}
         self.file_load(silent=True)
     def clear_menu(self):
         command=("cls"if os.name=="nt" else "clear")
         os.system(command)
         print("*" *50)
-    def file_intro(self):
+    def show_intro(self):
         print("-"*50)
         print("WELCOME TO DATABASE MANAGEMENT SYSTEM (JSON Based)")
-        print("Version v1.0.0")
-        print("-"*50)
+        print("Version v1.1")
+        print("="*50)
     def name_enter(self):
         name=input("Please enter your name :")
         if not name.replace(".","").replace(" ","").isalpha(): #this is added to to allow spaces and dots in names to reduce the invalid name entries and make it more user friendly
             print("Invalid name, Please try again")
             return
-        if name not in self.name_folder_1:
-            self.name_folder_1[name]={"ID" : self.id_save(), "Last saved" : self.time_save()}
+        if name not in self.records:
+            self.records[name]={"ID" : self.id_save(), "Last saved" : self.time_save()}
             self.save_names()
             print("Name and Unique ID added successfully")
         else:
             print("Name already exists, Please try again")
-        return self.name_folder_1
+        return self.records
     def time_save(self):
         time=datetime.datetime.now().isoformat(timespec="seconds")
         iso_time=str(time.replace("T","/"))
         return iso_time
-    def id_save(self):
+    def generate_id(self):
         id=uuid.uuid4().hex[:10]
         id_1=str(id)
         return id_1
-    def del_data(self):
-        user_folder=self.name_folder_1
-        delete=input("Do you wanna delete the entire data(enter 'entire data' or '0') or just a particular person's data (enter 'person' or '1') (Note: It is a permanent deletion, no way to recover the deleted data) :").lower()
+    def clear_proc(self):
+        print("Initialising File Deletion", end='',flush=True)
+        for i in range(3):
+            time.sleep(0.5)
+            print(".",end='',flush=True)
+    def delete_data(self):
+        user_folder=self.records
+        delete=input("Do you want to delete the entire data(enter 'entire data' or '0') or just a particular person's data (enter 'person' or '1') (Note: It is a permanent deletion, no way to recover the deleted data) :").lower()
         if delete=="entire data":
             print(f"Found Data :{user_folder}")
             input_y=input("Is this the data you want to delete (Yes/No) :").lower()
             if input_y=="yes":
-                print("Initialising File Deletion", end='',flush=True)
-                for i in range(3):
-                    time.sleep(0.5)
-                    print(".",end='',flush=True)
+                self.clear_proc()
                 try:
-                    self.name_folder_1.clear()
+                    self.records.clear()
                     self.save_names()
-                    print("Your data has been deleted entirely")
-                except FileNotFoundError:
-                    print("No data exists")
+                    print("Your data has been deleted successfully")
+                except FileNotFoundError as e:
+                    print(f"Error: {e}")
                 except Exception as e:
-                    print("An Unknown Error Occured, {e}")
-                    raise
+                    print(f"Error:,{e}")
             elif input_y=="no":
                 print("Deletion cancelled,Returning to the main menu")
                 return
@@ -70,20 +71,16 @@ class RecordManager():
                     print(f"Found Data :{user_folder[name]}")
                     input_x=input("Is this the data you want to delete (Yes/No) :").lower()
                     if input_x=="yes":
-                        print("Initialising File Deletion", end='',flush=True)
-                        for i in range(3):
-                            time.sleep(0.5)
-                            print(".",end='',flush=True)
+                        self.clear_proc()
                         try:
-                            self.name_folder_1[name].clear()
+                            self.records[name].clear()
                             self.save_names()
                             print(f"The data of the {name} has been deleted successfully")
                             print("Returning to main menu")
-                        except FileNotFoundError:
-                            print("No data exists")
+                        except FileNotFoundError as e:
+                            print(f"Error: No data exists, {e}")
                         except Exception as e:
-                            print("An Unknown Error Occured, {e}")
-                            raise
+                            print(f"Error: {e}")
                     elif input_x=="no":
                         print("Deletion cancelled, Returning to main menu")
                         return
@@ -94,40 +91,40 @@ class RecordManager():
                     print("Name not found, Try Again")
                 else:
                     print("Invalid Input, Try Again")
-            except FileNotFoundError:
-                print("Name not found, Try Again")
+            except FileNotFoundError as e:
+                print(f"Name not found, {e}")
     def search_name(self): # This function is used to search for a name and know whether it exists or not
         input_1=input("Do you want to search using ID or Name :").lower()
         if input_1=="name":
-                found=False
-                name=input("Please enter your name to search :")
-                if name in self.name_folder_1:
-                    user_input=self.name_folder_1[name]
-                    print(f"Name :", name)
-                    print("Unique ID found :",user_input.get("ID"))
-                    print(f"Last saved time :{user_input.get('Last saved')}")
-                    found=True
-                    if not found:
-                        print("ID not found, Try Again")
+            found=False
+            name=input("Please enter your name to search :")
+            if name in self.records:
+                user_input=self.records[name]
+                print(f"Name :", name)
+                print("Unique ID found :",user_input.get("ID"))
+                print(f"Last saved time :{user_input.get('Last saved')}")
+                found=True
+            else:
+                print("Name not found, Try Again")
         elif input_1=="id":
             id=input("Please enter your ID to search :")
             found=False
-            for name,data in self.name_folder_1.items():
+            for name,data in self.records.items():
                 if data["ID"] == id:
                     print(f"Name :", name)
                     print("Unique ID found :",data.get("ID"))
                     print(f"Last saved time :{data.get('Last saved')}")
                     found=True
-                if not found:
-                    print("ID not found, Try Again")
+            if not found:
+                print("ID not found, Try Again")
         else:
             print("Please enter a valid name or entry and try again")
             self.enter_direct
     def enter_direct(self):
         input_1=input("Do you want to open the file now ? (yes/no)").lower()
-        if input()=="yes":
+        if input_1=="yes":
                 self.file_load()
-        elif input()=="no":
+        elif input_1=="no":
             print("Okay, Returning to main menu")
             return
         else:
@@ -135,40 +132,40 @@ class RecordManager():
     def save_names(self): # This function is used to save the names to a json file and handle permission errors
         try:
             with open ("lost.json", "w") as f:
-                json.dump(self.name_folder_1,f)
-        except PermissionError:
-            print("Permission denied, cannot save the file")
+                json.dump(self.records,f,indent=4)
+        except PermissionError as e:
+            print(f" Error :Permission denied, {e}")
             return
-        except json.JSONDecodeError:
-            print("Error encoding JSON, Please try again")
+        except json.JSONDecodeError as e:
+            print(f"Error: failed encoding JSON, {e}")
             return
     def file_load(self,silent=False):
         try:
             with open("lost.json","r") as f:
-                name_folder_dict=json.load(f)
-                self.name_folder_1=name_folder_dict
+                records_dict=json.load(f)
+                self.records=records_dict
                 if not silent:
                     print("Loaded Names:")
-                    for name in self.name_folder_1:
-                        duplicate=self.name_folder_1[name]
+                    for name in self.records:
+                        duplicate=self.records[name]
                         print("Your name is :",name)
                         print("Your unique ID is :",duplicate["ID"])
                         print ("last saved time :",duplicate["Last saved"])
                         print("Your file has been opened successfully")
-        except FileNotFoundError:
+        except FileNotFoundError as e:
             if not silent:
-                print("File not found, Please try again")
+                print(f"Error: File not found, {e}")
             return
-        except json.JSONDecodeError:
+        except json.JSONDecodeError as e:
             if not silent:
-                print("Error decoding JSON, Please try again")
+                print(f"Error: failed decoding JSON, {e}")
             return
     def qr_code(self):
         name=input("Please enter your name to search and provide the details using QR code :")
-        if name not in self.name_folder_1:
+        if name not in self.records:
             print("Your name is not available in our databases")
             return
-        details_1=self.name_folder_1[name]
+        details_1=self.records[name]
         details_2=details_1["ID"]
         details_3=details_1["Last saved"]
         main_details=(f"Name : {name}\n ID={details_2}\n Last saved={details_3}")
@@ -182,10 +179,10 @@ class RecordManager():
             if os.path.exists(file):
                 os.remove(file)
         except Exception as e:
-            print(f"Unable to open the image viewer. Please open '{unique_id}_qr.png' manually to view the QR code.")
+            print(f"Error: {e}")
     def menu(self): #This is the menu part where the options can be accessed
         while True:
-            print("^"*15+"MENU for DATABASE MANAGEMENT SYSTEM"+"^"*15)
+            print("-"*15+"MENU for DATABASE MANAGEMENT SYSTEM"+"-"*15)
             print("1-Enter name")
             print("2-Search Name")
             print("3-Save Names")
@@ -214,9 +211,10 @@ class RecordManager():
             elif choice==6:
                 self.qr_code()
             elif choice==7:
-                self.del_data()
-        print("Invalid choice, Please try again")
-        return
+                self.delete_data()
+            else:
+                print("Invalid choice, Please try again")
+                continue
 manager=RecordManager()
-manager.file_intro()
+manager.show_intro()
 manager.menu()
